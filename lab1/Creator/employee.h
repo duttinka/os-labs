1c4536e339c23613b8576298376ea3af55cfc6ad
ytenok228 <duttinka@gmail.com>
2025-06-08 14:38:01 +0300

    docs: Add comments, README and build script

--- lab1/Creator/employee.h
+++ lab1/Creator/employee.h
@@ -1,1 +1,10 @@
+
 #pragma once
+#include <string>
+
+// Defines the structure for an employee record
+struct employee {
+  int num;        // Employee ID number
+  char name[10];  // Employee name
+  double hours;   // Hours worked
+};
--- lab1/Main/employee.h
+++ lab1/Main/employee.h
@@ -1,1 +1,9 @@
 #pragma once
+#include <string>
+
+// Defines the structure for an employee record
+struct employee {
+  int num;        // Employee ID number
+  char name[10];  // Employee name
+  double hours;   // Hours worked
+};
